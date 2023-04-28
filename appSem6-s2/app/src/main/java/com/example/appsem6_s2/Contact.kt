067a0fc227d4282e18602d6dc7660acd63df0b92
MICHAELCARRILLO <89095546+MICHAELCARRILLO@users.noreply.github.com>
2023-04-28 12:37:20 -0500

    feat: Added room project files

---
+++ appSem6-s2/app/src/main/java/com/example/appsem6_s2/Contact.kt
@@ -0,0 +1,15 @@
+package com.example.appsem6_s2
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity
+class Contact (
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+    @ColumnInfo
+    var name: String,
+    @ColumnInfo
+    var phone: String
+    )
